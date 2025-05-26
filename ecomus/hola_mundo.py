57b1448e64234e3e1ffbc340c018116f45b96101
PedroRamirez01 <85964832+PedroRamirez01@users.noreply.github.com>
2025-05-25 22:16:43 -0400

    Remove unnecessary metadata comments from hola_mundo.py

--- ecomus/hola_mundo.py
+++ ecomus/hola_mundo.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-# @Time    : 2023/10/8 17:00
-# @Author  : ecomus
-# @File    : hola_mundo.py
-# @Software: PyCharm
-
 def hola_mundo():
     print("Hola Mundo")
     return "Hola Mundo"
