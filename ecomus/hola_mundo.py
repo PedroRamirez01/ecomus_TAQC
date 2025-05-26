c3ddd540bb18c0cffdce54dbf6850459ee9e563f
PedroRamirez01 <85964832+PedroRamirez01@users.noreply.github.com>
2025-05-25 22:10:16 -0400

    Refactor hola_mundo.py to define a function and remove redundant print statements

--- ecomus/hola_mundo.py
+++ ecomus/hola_mundo.py
@@ -1,3 +1,13 @@
-print("HOLAMUNDO")
-print("Hola Mundo")
-print("Hola Mundo")
+# -*- coding: utf-8 -*-
+# @Time    : 2023/10/8 17:00
+# @Author  : ecomus
+# @File    : hola_mundo.py
+# @Software: PyCharm
+
+def hola_mundo():
+    print("Hola Mundo")
+    return "Hola Mundo"
+
+if __name__ == "__main__":
+    hola_mundo()
+    # Test the function
